2a55bc317d7c966724984c48dfc2627fc390233b
Swee <swee@umich.edu>
2018-12-24 11:32:37 -0500

    Monster chasing player

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Graphics.Gloss.Rendering
 import Graphics.Gloss.Data.Picture
 import Graphics.Gloss.Data.Color
 import System.Exit (exitSuccess)
+import System.Random
 import Control.Concurrent (threadDelay)
 import Control.Monad (when, unless) 
 import Control.Arrow
@@ -38,7 +39,7 @@ windowHeight = 480
 -- Game specific data types
 
 -- Type aliases
-type Pos           = Point
+type Position      = Point
 type UserCommands  = (Bool,Bool,Bool,Bool)
 
 -- Enumerations
@@ -47,30 +48,37 @@ data Direction     = WalkUp | WalkDown | WalkLeft | WalkRight
 
 
 data Player        = Player {
-                               position::Pos
+                               positionP::Position
                             }
 data Monster       = Monster {
-                               position::Pos
+                               positionM::Position
                               ,monsterStatus::MonsterStatus
                              }
 
 -- Signal inputs and outputs
-Data Input  = Input {
-                       userInput    :: Frp.Event UserCommands
-                      ,monsterInput :: Int
-                    }
+data Input  = Input {
+                       userInput  :: Frp.Event UserCommands
+                      ,monsterInput::Int
+                    } deriving (Show)
+ 
 
-Data Output = Output {
+data Output = Output {
                        getPlayer  :: Player
                       ,getMonster :: Monster
                       }
 
 
+initPlayer::Player 
+initPlayer = Player (10,10)
+
+initMonster::Monster
+initMonster = Monster (50,50) (Wander WalkUp 1)
+
 main :: IO ()
 main = do
   glossState <- initState
   withWindow windowWidth windowHeight "Resurrection" $ \win -> do 
-    rh <- Frp.reactInit (getInput win) (renderFrame win glossState) movement 
+    rh <- Frp.reactInit (getInput win) (renderFrame win glossState) (gameEvolution initPlayer initMonster)
     loop glossState win rh
     exitSuccess
   where 
@@ -82,12 +90,15 @@ main = do
       flag <- Frp.react rh (1,Just (input))
       unless k $ loop glossState window rh 
 
-renderFrame::Window->State->Frp.ReactHandle Inp Out -> Bool -> Out -> IO Bool
+renderFrame::Window->State->Frp.ReactHandle Input Output -> Bool -> Output -> IO Bool
 renderFrame window glossState rh ch out = do
-  let posx = fst $ position out
-  let posy = snd $ position out
+  let posx = fst . positionP . getPlayer  $ out
+  let posy = snd . positionP . getPlayer  $ out
+  let monx = fst . positionM . getMonster $ out
+  let mony = snd . positionM . getMonster $ out
   displayPicture (windowWidth, windowHeight) white glossState 1.0 $ 
-    Pictures [Color red $ translate posx posy $ circleSolid 30]
+    Pictures [Color red $ translate posx posy $ circleSolid 30
+             ,Color blue $ translate monx mony $ rectangleSolid 15 15]
   swapBuffers window
   return True
 
@@ -99,22 +110,26 @@ isPress KeyState'Pressed   = True
 isPress KeyState'Repeating = True 
 isPress _                  = False
 
-getInput::Window->IO (Frp.Event Inpdirection)
+getInput::Window->IO Input 
 getInput window = do 
       l <- keyIsPressed window Key'Left
       r <- keyIsPressed window Key'Right
       u <- keyIsPressed window Key'Up
       d <- keyIsPressed window Key'Down
-      return (Frp.Event (l,r,u,d))
+      m <- randomRIO (1,4)
+      let inputData = Input (Frp.Event (l,r,u,d)) m
+      return inputData 
 
-movement::Frp.SF Inp Out 
-movement = proc i -> do
+gameEvolution::Player->Monster->Frp.SF Input Output
+gameEvolution initPlayer initMonster = proc i -> do
    rec 
-       pos <- Frp.drSwitch (Frp.constant (Player (0.0,0.0),(False,False,False,False))) >>^ updatePos -< (i,newEvent)
-
-       let posInputs = Frp.fromEvent i
-           newEvent  = i `Frp.tag` (Frp.constant (pos,posInputs))
-   Frp.returnA -< pos
+       playr     <- Frp.drSwitch (Frp.constant (initPlayer,(False,False,False,False))) >>^ updatePos -< (userInput i,newEvent)
+       monIS     <- Frp.iPre (0,initMonster) -< (monsterInput i,monstr)
+       monstr    <- arr updateMonsterState -< (fst monIS,snd monIS,playr)
+       let posInputs = Frp.fromEvent $ userInput i
+           newEvent  = userInput i `Frp.tag` (Frp.constant (playr,posInputs))
+           outSignal = Output playr monstr  
+   Frp.returnA -< outSignal 
 
 updatePos (oldPos,newInput) = computeNewState oldPos newInput
 
@@ -124,3 +139,29 @@ computeNewState  (Player (x,y)) (_,True,_,_) = Player (x+10,y)
 computeNewState  (Player (x,y)) (_,_,True,_) = Player (x,y+10)
 computeNewState  (Player (x,y)) (_,_,_,True) = Player (x,y-10)
 computeNewState  (Player (x,y)) (False,False,False,False) = Player (x,y)
+
+updateMonsterState::(Int,Monster,Player)->Monster
+updateMonsterState (val,monstr,playr) = newMonster 
+    where
+      newMonster = Monster (mx+dx,my+dy) monstatus  
+      (px,py) = (fst . positionP $ playr, snd . positionP $ playr)
+      (mx,my) = (fst . positionM $ monstr, snd . positionM $ monstr)
+      (dx,dy) = if monsterIsNear 
+                then ((px - mx)*0.002,(py - my)*0.002)
+                else wanderRandomnly  
+      monsterIsNear = (sqrt $ (px - mx)^2 + (py - my)^2) < 100
+      monstatus = if monsterIsNear
+                  then Hunting
+                  else
+                    case val of
+                      1 -> Wander WalkUp 2
+                      2 -> Wander WalkDown 2
+                      3 -> Wander WalkLeft 2
+                      4 -> Wander WalkRight 2
+                      _ -> Wander WalkRight 0 
+      wanderRandomnly = case monstatus of
+                          Wander WalkUp val    -> (0,fromIntegral val) 
+                          Wander WalkDown val  -> (0,fromIntegral (-val))
+                          Wander WalkLeft val  -> (fromIntegral (-val),0)
+                          Wander WalkRight val -> (fromIntegral val,0)
+                         
